64fd08e1ecb83d645943cd2ede572601cfa43db6
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-10-07 19:47:23 +0000

    wip

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,20 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
+vim.g.nvim_tree_show_icons = {
+  folders = 0,
+  files = 0,
+  git = 0,
+  folder_arrows = 0,
+}
+
+require('nvim-tree').setup({
+  update_focused_file = {
+    enable = true,
+  },
+  view = {
+    width = 50,
+  },
+})
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "ruby", "rust", "vim" },
+  ensure_installed = { "javascript", "typescript", "lua", "vim" },
   sync_install = false,
   auto_install = true,
   highlight = {
